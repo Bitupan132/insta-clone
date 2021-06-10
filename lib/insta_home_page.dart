9357099a8dd9cec76b4db38ed8b3988e833cb9be
Bitupan132 <bitupanarandhara7@gmail.com>
2021-06-10 12:33:24 +0530

    app bar and bottom app bar added

---
+++ lib/insta_home_page.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class InstaHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          elevation: 0,
+          centerTitle: true,
+          leading: Image.asset(
+            "assets/images/add_insta.png",
+          ),
+          title: SizedBox(
+            height: 35,
+            child: Image.asset("assets/images/insta.png"),
+          ),
+          actions: [
+            Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: Image.asset(
+                "assets/images/insta_mess.png",
+                height: 20,
+                width: 22,
+              ),
+            ),
+          ],
+        ),
+        bottomNavigationBar: BottomAppBar(
+          child:
+              Row(mainAxisAlignment: MainAxisAlignment.spaceAround, children: [
+            IconButton(
+                onPressed: null,
+                icon: Icon(
+                  Icons.home_filled,
+                  color: Colors.black,
+                )),
+            IconButton(
+                onPressed: null,
+                icon: Icon(
+                  CupertinoIcons.search,
+                  color: Colors.black,
+                )),
+            IconButton(
+                onPressed: null,
+                icon: Icon(
+                  Icons.movie,
+                  color: Colors.black,
+                )),
+            IconButton(
+              onPressed: null,
+              icon: Icon(
+                Icons.favorite_border_rounded,
+                color: Colors.black,
+              ),
+            ),
+            IconButton(
+                onPressed: null,
+                icon: Icon(
+                  Icons.account_circle_outlined,
+                  color: Colors.black,
+                )),
+          ]),
+        ));
+  }
+}
